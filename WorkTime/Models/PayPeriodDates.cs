fa0fb16aa1f3fb1ff7a63476124e361fee227669
Jason <jasryoung@hotmail.com>
2018-04-04 21:06:52 -0400

    Started Model for PayPeriodDates Started HomeController

---
+++ WorkTime/Models/PayPeriodDates.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WorkTime.Models
+{
+    public class PayPeriodDates
+    {
+        public DayOfWeek PayDay { get;  set; } //day of week employee is paid
+        public string PayPeriod { get; set; } //periodicity of pay
+        public DayOfWeek FirstDayOfPeriod { get; set; }
+
+
+        public List<string> DatesForWeek()
+        {
+            DayOfWeek day = DateTime.Now.DayOfWeek;
+            return new List<string>();
+        }
+    }
+}
